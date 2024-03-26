fae59109d95caed84d4ba1e391dd41ec68f287de
jones58 <108466184+jones58@users.noreply.github.com>
2024-03-26 15:59:57 +0000

    edit text

--- src/components/AroundMuseum.tsx
+++ src/components/AroundMuseum.tsx
@@ -3,12 +3,13 @@ import Carousel from "./imageCarousel";
 const VisitorInfo = () => {
   return (
     <section className="py-8">
-      <div>
+      <div className="flex flex-col sm:flex-row justify-between">
         <h1 className="text-2xl font-bold py-5 text-black">
           Around the Museum
         </h1>
-        <Carousel />
+        <h1 className="text-2xl font-bold py-5 text-black">Map</h1>
       </div>
+      <Carousel />
     </section>
   );
 };
--- src/components/VisitorInfo.tsx
+++ src/components/VisitorInfo.tsx
@@ -1,13 +1,22 @@
+import Clock from "./Clock";
+
 const VisitorInfo = () => {
   return (
-    <section className="py-8 text-black">
-      <div>
-        <h1 className="text-2xl font-bold py-5">Your visit</h1>
+    <section className="py-8 text-black flex flex-col sm:flex-row justify-between">
+      <h1 className="text-2xl font-bold py-5">Your visit</h1>
+      <div className="py-8">
         <p className="text-lg font-mono">The museum is open today</p>
-        <p className="text-lg font-mono">The museum is open today</p>
-        <p className="text-lg font-mono">The museum is open today</p>
-        <p>Insert clock here</p>
+        <p className="text-lg font-mono">
+          The museum is not open tomorrow
+        </p>
+        <p className="text-lg font-mono">
+          The museum is open on Friday
+        </p>
+        <p className="text-lg font-mono">
+          The museum is open on Saturday
+        </p>
       </div>
+      <Clock />
     </section>
   );
 };
