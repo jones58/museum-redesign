92b6a16e1da699d004b31868d7fa4f5cb4bb2c6a
jones58 <108466184+jones58@users.noreply.github.com>
2024-03-27 15:40:16 +0000

    change look of image text card component

--- src/components/imagetextCard.tsx
+++ src/components/imagetextCard.tsx
@@ -12,17 +12,19 @@ const ImageTextCard = ({
   link: string;
 }) => {
   return (
-    <section className={`${direction} flex sm:h-[33vh]`}>
-      <div className="bg-museumGreen text-black w-1/2 text-xl sm:text-2xl font-mono sm:h-[33vh]">
+    <section
+      className={`${direction} flex justify-between sm:h-[33vh] rounded-lg`}
+    >
+      <div className="bg-museumGreen text-black text-xl sm:text-2xl sm:h-[33vh] w-1/2">
         <a href={link} target="_blank">
-          <h1 className="p-5 sm:p-10">{text}</h1>
+          <h1 className="p-5 sm:p-10 font-sans">{text}</h1>
         </a>
       </div>
-      <div className="bg-pink-500 text-black w-1/2 text-2xl font-mono sm:h-[33vh]">
+      <div className="bg-pink-500 text-black w-1/2 h-auto text-2xl font-mono">
         <img
           src={imageSrc}
           alt={imageAlt}
-          className="overflow-clip w-full h-full object-cover"
+          className="w-full h-full object-cover"
         />
       </div>
     </section>
