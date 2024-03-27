2d6bfa20e8d6097c204ea7f38feca08861c9d494
jones58 <108466184+jones58@users.noreply.github.com>
2024-03-27 16:05:20 +0000

    edit welcome text

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -3,8 +3,8 @@ import ImageTextCard from "./imagetextCard";
 const Welcome = () => {
   return (
     <ImageTextCard
-      text="The world's largest collection
-      of working historic computers"
+      text="Visit the world's largest collection
+      of working historic computers."
       imageSrc="images/welcome.jpg"
       imageAlt="The large systems gallery at the Museum"
       direction="flex-row"
