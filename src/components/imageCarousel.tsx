4a483d051151b17b5e923149853e3129d5905fa7
jones58 <108466184+jones58@users.noreply.github.com>
2024-03-27 16:04:53 +0000

    edit padding

--- src/components/imageCarousel.tsx
+++ src/components/imageCarousel.tsx
@@ -20,17 +20,23 @@ export default function Carousel({
   });
 
   return (
-    <div ref={ref} className="keen-slider">
-      {imageSrcList.map((imageSrc: string, index: number) => (
-        <div key={index} className="keen-slider__slide number-slide1">
-          <img
-            src={imageSrcList[index]}
-            alt={imageAltList[index]}
-            className="h-[30vh] w-full object-cover rounded-lg"
-          />
-          <h1>{titleList[index]}</h1>
-        </div>
-      ))}
+    <div>
+      <div ref={ref} className="keen-slider">
+        {imageSrcList.map((imageSrc: string, index: number) => (
+          <div
+            key={index}
+            className="keen-slider__slide number-slide1"
+          >
+            <img
+              src={imageSrcList[index]}
+              alt={imageAltList[index]}
+              className="h-[30vh] w-full object-cover rounded-lg"
+            />
+            <h1 className="pt-2">{titleList[index]}</h1>
+          </div>
+        ))}
+      </div>
+      <h1 className="text-right">Scroll icon here</h1>
     </div>
   );
 }
